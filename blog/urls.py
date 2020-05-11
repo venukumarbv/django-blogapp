2ac39c349a5cc72953d16c95e5b0053c3a541711
venukumar <VKvision@venu.com>
2020-05-11 11:38:16 +0530

    First Commit

---
+++ blog/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeletelView,
+    UserPostListView
+)
+from . import views
+
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog_home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'), #temple <post>_form.html
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeletelView.as_view(), name='post-delete'),
+    path('about/', views.about, name='blog_about'),
+]
+
+#<app><model>_<viewtype>.html
